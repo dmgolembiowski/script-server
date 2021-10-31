0cd0576009ddbaec9dc64af6b742fd4244daf26f
David Golembiowski <david@dgolembiowski.com>
2021-10-31 18:58:35 -0400

    Bumping this to python3-compatible syntax

--- samples/scripts/mult words.py
+++ samples/scripts/mult words.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 
-print
-'hello, your words are:'
-for word in sys.argv:
-    print '- ' + word
+print('hello, your words are: ' + '-'.join(sys.argv[1:]))
+#for word in sys.argv:
+#    print '- ' + word
